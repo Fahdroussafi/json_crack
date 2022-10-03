f938f8b3eb925a13db751bb73d67f96efe69d833
VAFAGIT <belgazzarwafae@gmail.com>
2022-10-03 11:22:30 +0100

    v1

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,4 +13,20 @@ router.post("/", async (req, res) => {
     res.json("SUCCESS");
   });
 });
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ where: { username: username}  });
+  
+  if (!user) res.json({ error: "User Doesn't Exist" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) res.json({ error: "Wrong Username And Password Combination" });
+
+    res.json("YOU LOGGED IN!!")
+  });
+
+});
+
 module.exports = router;
