e3cc917b993017785fe32ba336303f7e54b0d65e
Fahdroussafi <cmpunk589@outlook.com>
2022-10-03 14:06:25 +0100

    fix navbar/home

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,22 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Graph";
 import Login from "./pages/Login";
 // import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className="navbar">
-          <div className="navbar bg-base-100">
-            <div className="flex-1">
-              <Link to="/" className="btn btn-ghost normal-case text-xl">
-                Home
-              </Link>
-            </div>
-            <div className="flex-none">
-              <ul className="menu menu-horizontal p-0">
-                <li>
-                  <Link to="/login"> Login</Link>
-                </li>
-
-                <li>
-                  <Link to="/registration"> Registration</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          {/* <Route path="/registration" exact component={Registration} /> */}
-          <Route path="/login" exact component={Login} />
-        </Switch>
+        <Route exact path="/">
+          <Navbar />
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        {/* <Route path="/registration">
+          <Register />
+        </Route> */}
       </Router>
     </div>
   );
