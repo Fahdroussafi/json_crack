2efaea61b7f432eb74e3cfc186f41a11e6c752c5
Fahdroussafi <cmpunk589@outlook.com>
2022-10-04 08:41:14 +0100

    add JWT

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 
+const { sign } = require("jsonwebtoken");
+
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
   bcrypt.hash(password, 10).then((hash) => {
@@ -17,16 +19,19 @@ router.post("/", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await Users.findOne({ where: { username: username}  });
-  
+  const user = await Users.findOne({ where: { username: username } });
+
   if (!user) res.json({ error: "User Doesn't Exist" });
 
   bcrypt.compare(password, user.password).then((match) => {
     if (!match) res.json({ error: "Wrong Username And Password Combination" });
 
-    res.json("YOU LOGGED IN!!")
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "secretkey"
+    );
+    res.json(accessToken);
   });
-
 });
 
 module.exports = router;
