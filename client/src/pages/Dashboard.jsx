619c3f6d446efedeb0998b9a6e223ba8b098f30a
Fahd Roussafi <93979909+Fahdroussafi@users.noreply.github.com>
2022-10-06 09:17:55 +0100

    UI final updtae

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -26,50 +26,47 @@ const Dashboard = () => {
           li.innerHTML += `: ${data[key]}`; // append value to key if not an object (leaf node)
         }
         ul.appendChild(li);
-
-        li.style.color = typeof data[key] === "string" ? "green" : "blue";
-        li.style.fontWeight = typeof data[key] === "string" ? "bold" : "normal";
+        li.style.color = typeof data[key] === "string" ? "red" : "white"; // change color if leaf node (string) or not (object)
+        li.style.fontWeight =
+          typeof data[key] === "string" ? "extrabold" : "bold";
         li.style.listStyleType =
-          typeof data[key] === "string" ? "none" : "circle";
+          typeof data[key] === "string" ? "none" : "disc";
         li.style.marginLeft = typeof data[key] === "string" ? "0px" : "20px";
         li.style.marginBottom = typeof data[key] === "string" ? "0px" : "10px";
-        li.style.fontSize = typeof data[key] === "string" ? "16px" : "14px";
+        li.style.fontSize = typeof data[key] === "string" ? "20px" : "16px";
         li.style.fontFamily =
           typeof data[key] === "string" ? "monospace" : "sans-serif";
         li.style.textAlign = typeof data[key] === "string" ? "left" : "center";
         li.style.padding = typeof data[key] === "string" ? "0px" : "5px";
-        li.style.borderRadius = typeof data[key] === "string" ? "0px" : "5px";
+        li.style.borderRadius = typeof data[key] === "string" ? "0px" : "10px";
         li.style.backgroundColor =
-          typeof data[key] === "string" ? "transparent" : "#f5f5f5";
+          typeof data[key] === "string" ? "transparent" : "";
         li.style.border =
-          typeof data[key] === "string" ? "none" : "1px solid #e5e5e5";
-        li.style.userSelect = typeof data[key] === "string" ? "none" : "auto";
-        li.style.textDecoration =
-          typeof data[key] === "string" ? "none" : "underline";
-        li.style.textDecorationColor =
-          typeof data[key] === "string" ? "none" : "#e5e5e5";
-        li.style.textDecorationStyle =
-          typeof data[key] === "string" ? "none" : "dotted";
-        li.style.textDecorationThickness =
-          typeof data[key] === "string" ? "none" : "1px";
+          typeof data[key] === "string" ? "none" : "2px solid #e5e5e5";
       }
       return ul;
-      // li.innerHTML = `<span class="caret">${key}</span>`;
-      // if (typeof data[key] === "object") {
-      //   // console.log(key);
-      //   li.appendChild(createTree(data[key]));
-      // } else {
-
-      //   // li.innerHTML = `<span>${key}: ${data[key]}</span>`;
-      //   // console.log(key, ":", data[key]);
-      // }
-
-      // ul.appendChild(li);
     };
   };
+  // li.innerHTML = `<span class="caret">${key}</span>`;
+  // if (typeof data[key] === "object") {
+  //   // console.log(key);
+  //   li.appendChild(createTree(data[key]));
+  // } else {
+
+  //   // li.innerHTML = `<span>${key}: ${data[key]}</span>`;
+  //   // console.log(key, ":", data[key]);
+  // }
+
+  // ul.appendChild(li);
+
+  const clearList = () => {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+  };
+
   return (
     <>
-      <div className="hero min-h-screen bg-white ">
+      <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl text-white font-extrabold">
@@ -91,7 +88,7 @@ const Dashboard = () => {
             <div className="mb-5">
               <button
                 className="btn btn-outline text-purple-600 font-bold hover:bg-transparent hover:text-white border-2 border-purple-600"
-                // onClick={clearList}
+                onClick={clearList}
               >
                 Clear List
               </button>
